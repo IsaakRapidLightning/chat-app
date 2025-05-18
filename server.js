da33b5fe6cadbfde140f235191d11f4c51aefa21
NewtonsWorkspace <isaak@clement-family.us>
2025-05-17 22:36:45 -0500

    Add friend code chat feature

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const { randomBytes } = require('crypto');
 
 const app = express();
 const server = http.createServer(app);
@@ -8,18 +9,40 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+const PORT = process.env.PORT || 3000;
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // broadcast to all
+// Map friend codes to socket ids
+const friendCodeToSocket = new Map();
+
+io.on('connection', (socket) => {
+  let myFriendCode = null;
+
+  // Generate and send a new friend code
+  socket.on('generateCode', () => {
+    // Generate 6-char alphanumeric code
+    myFriendCode = randomBytes(3).toString('hex');
+    friendCodeToSocket.set(myFriendCode, socket.id);
+    socket.emit('codeGenerated', myFriendCode);
   });
 
+  // Handle sending a message to a friend code
+  socket.on('sendMessage', ({ toCode, message }) => {
+    const targetSocketId = friendCodeToSocket.get(toCode);
+    if (targetSocketId) {
+      io.to(targetSocketId).emit('receiveMessage', { fromCode: myFriendCode, message });
+    } else {
+      socket.emit('receiveMessage', { fromCode: 'System', message: `Friend code ${toCode} not found.` });
+    }
+  });
+
+  // Clean up on disconnect
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    if (myFriendCode) {
+      friendCodeToSocket.delete(myFriendCode);
+    }
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
